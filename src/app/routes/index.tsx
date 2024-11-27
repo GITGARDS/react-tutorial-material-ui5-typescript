a283b06529a4be9d7276cb84b3da66fdef2d3422
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2024-11-27 20:46:44 -0300

    feat: React, Material UI 5 e Typescript: #04 - Instalando Material UI V5

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-initial" element={<p>Pagina inicial</p>} />
+      <Route path="/pagina-initial" element={<Button variant="contained">Teste</Button>} />
       <Route path="*" element={<Navigate to="/pagina-initial" />} />
     </Routes>
   );
