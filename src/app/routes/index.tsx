bc280f352668dec994c8a8c01af1154e5c5c2397
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2024-11-28 09:51:46 -0300

    feat: React, Material UI 5 e Typescript: #06 - Configurando tema escuro e a troca de tema

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
   return (
     <Routes>
-      <Route path="/pagina-initial" element={<Button variant="contained" color="primary">Teste</Button>} />
+      <Route
+        path="/pagina-initial"
+        element={
+          <Button
+            onClick={toggleTheme}
+            variant="contained"
+            color="primary"
+          >
+            Toggle theme
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/pagina-initial" />} />
     </Routes>
   );
