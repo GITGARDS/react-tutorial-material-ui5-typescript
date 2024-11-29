b08e2058de09f9277ba132b7a48a24407784d14d
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2024-11-29 19:10:12 -0300

    feat: React, Material UI 5 e Typescript: #27 - Adicionando tabela para listagem de pessoas

--- src/app/pages/pessoas/ListagemDePessoas.tsx
+++ src/app/pages/pessoas/ListagemDePessoas.tsx
@@ -1,27 +1,46 @@
-import { useEffect, useMemo } from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { FerramentasDaListagem } from "../../shared/components";
 import { useDebounce } from "../../shared/hooks";
 import { LayoutBaseDePagina } from "../../shared/layouts";
-import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import {
+  IListagemPessoa,
+  PessoasService,
+} from "../../shared/services/api/pessoas/PessoasService";
 
 export const ListagemDePessoas = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(500, false);
 
-  const { debounce } = useDebounce(3000, false);
+  const [rows, setRows] = useState<IListagemPessoa[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const busca = useMemo(() => {
     return searchParams.get("busca") || "";
   }, [searchParams]);
 
   useEffect(() => {
+    setIsLoading(true);
     debounce(() => {
       PessoasService.getAll(1, busca).then((result) => {
+        setIsLoading(false);
         if (result instanceof Error) {
           alert(result.message);
-          return;
+        } else {
+          console.log(result);
+          setTotalCount(result.totalCount);
+          setRows(result.data);
         }
-        console.log(result);
       });
     });
   }, [busca]);
@@ -40,7 +59,33 @@ export const ListagemDePessoas = () => {
         />
       }
     >
-      Testando
+      <TableContainer
+        component={Paper}
+        variant="outlined"
+        sx={{ m: 1, width: "auto" }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>{row.id}</TableCell>
+                <TableCell>{row.nomeCompleto}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </LayoutBaseDePagina>
   );
 };
