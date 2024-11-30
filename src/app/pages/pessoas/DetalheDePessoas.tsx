0307b14352c23dd66f6ef051f8c27cb91865b58d
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2024-11-30 19:02:55 -0300

    feat: React, Material UI 5 e Typescript: #38 - Como validar formulários no React

--- src/app/pages/pessoas/DetalheDePessoas.tsx
+++ src/app/pages/pessoas/DetalheDePessoas.tsx
@@ -1,9 +1,10 @@
 import { Box, Grid, LinearProgress, Paper, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import * as yup from "yup";
 
 import { FerramentasDeDetalhe } from "../../shared/components";
-import { VForm, VTextField, useVForm } from "../../shared/forms";
+import { IVFormErrors, VForm, VTextField, useVForm } from "../../shared/forms";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 
@@ -12,6 +13,11 @@ interface IFormData {
   cidadeId: number;
   nomeCompleto: string;
 }
+const formValidationSchema: yup.SchemaOf<IFormData> = yup.object().shape({
+  cidadeId: yup.number().required(),
+  email: yup.string().required().email(),
+  nomeCompleto: yup.string().required().min(3),
+});
 
 export const DetalheDePessoas: React.FC = () => {
   const { formRef, save, saveAndClose, isSaveAndClose } = useVForm();
@@ -46,37 +52,53 @@ export const DetalheDePessoas: React.FC = () => {
   }, [id]);
 
   const handleSave = (dados: IFormData) => {
-    setIsLoading(true);
+    formValidationSchema
+      .validate(dados, { abortEarly: false })
+      .then((dadosValidados) => {
+        setIsLoading(true);
 
-    if (id === "nova") {
-      PessoasService.create(dados).then((result) => {
-        setIsLoading(false);
+        if (id === "nova") {
+          PessoasService.create(dadosValidados).then((result) => {
+            setIsLoading(false);
 
-        if (result instanceof Error) {
-          alert(result.message);
-        } else {
-          if (isSaveAndClose()) {
-            navigate("/pessoas");
-          } else {
-            navigate(`/pessoas/detalhe/${result}`);
-          }
-        }
-      });
-    } else {
-      PessoasService.updateById(Number(id), { id: Number(id), ...dados }).then(
-        (result) => {
-          setIsLoading(false);
-
-          if (result instanceof Error) {
-            alert(result.message);
-          } else {
-            if (isSaveAndClose()) {
-              navigate("/pessoas");
+            if (result instanceof Error) {
+              alert(result.message);
+            } else {
+              if (isSaveAndClose()) {
+                navigate("/pessoas");
+              } else {
+                navigate(`/pessoas/detalhe/${result}`);
+              }
             }
-          }
+          });
+        } else {
+          PessoasService.updateById(Number(id), {
+            id: Number(id),
+            ...dadosValidados,
+          }).then((result) => {
+            setIsLoading(false);
+
+            if (result instanceof Error) {
+              alert(result.message);
+            } else {
+              if (isSaveAndClose()) {
+                navigate("/pessoas");
+              }
+            }
+          });
         }
-      );
-    }
+      })
+      .catch((errors: yup.ValidationError) => {
+        const validationErrors: IVFormErrors = {};
+
+        errors.inner.forEach((error) => {
+          if (!error.path) return;
+
+          validationErrors[error.path] = error.message;
+        });
+
+        formRef.current?.setErrors(validationErrors);
+      });
   };
 
   const handleDelete = (id: number) => {
--- src/app/shared/forms/index.ts
+++ src/app/shared/forms/index.ts
@@ -1,5 +1,5 @@
-export * from "./useVForm";
-export * from "./VForm";
-export * from "./VScope";
-export * from "./VTextField";
-
+export * from './IVFormErrors';
+export * from './useVForm';
+export * from './VForm';
+export * from './VScope';
+export * from './VTextField';
