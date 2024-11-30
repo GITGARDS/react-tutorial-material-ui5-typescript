8552657e596cdf3b9507a71bc5a29ec6ef6b0f8d
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2024-11-30 18:32:57 -0300

    feat: React, Material UI 5 e Typescript: #36 - Como usar layout Grid do Material UI no formulário

--- src/app/pages/pessoas/DetalheDePessoas.tsx
+++ src/app/pages/pessoas/DetalheDePessoas.tsx
@@ -1,15 +1,13 @@
-import { LinearProgress } from "@mui/material";
+import { Box, Grid, LinearProgress, Paper, Typography } from "@mui/material";
 import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+
 import { FerramentasDeDetalhe } from "../../shared/components";
-import { Environment } from "../../shared/environment";
 import { VTextField } from "../../shared/forms";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
-
-interface IDetalheDePessoasProps {}
 
 interface IFormData {
   email: string;
@@ -17,122 +15,143 @@ interface IFormData {
   nomeCompleto: string;
 }
 
-export const DetalheDePessoas: React.FC<IDetalheDePessoasProps> = () => {
+export const DetalheDePessoas: React.FC = () => {
   const { id = "nova" } = useParams<"id">();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [nome, setNome] = useState("");
-
   const navigate = useNavigate();
 
   const formRef = useRef<FormHandles>(null);
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState("");
+
   useEffect(() => {
     if (id !== "nova") {
       setIsLoading(true);
+
       PessoasService.getById(Number(id)).then((result) => {
         setIsLoading(false);
+
         if (result instanceof Error) {
           alert(result.message);
           navigate("/pessoas");
         } else {
-          formRef.current?.setData(result)          
+          setNome(result.nomeCompleto);
+          formRef.current?.setData(result);
         }
       });
-    } else {
-      setIsLoading(true);
     }
-  }, [id, isLoading]);
+  }, [id]);
 
   const handleSave = (dados: IFormData) => {
+    setIsLoading(true);
+
     if (id === "nova") {
-      setIsLoading(true);
       PessoasService.create(dados).then((result) => {
         setIsLoading(false);
+
         if (result instanceof Error) {
           alert(result.message);
         } else {
-          navigate(`/pessoas/detalhe/${result}`);          
-          alert(Environment.ADCIONADO_COM_SUCESSO);
+          navigate(`/pessoas/detalhe/${result}`);
         }
       });
     } else {
-      setIsLoading(true);
       PessoasService.updateById(Number(id), { id: Number(id), ...dados }).then(
         (result) => {
           setIsLoading(false);
+
           if (result instanceof Error) {
             alert(result.message);
-          } else {
-            alert(Environment.ALTERADO_COM_SUCESSO);
           }
         }
       );
     }
   };
 
-  const handleDelete = useCallback(
-    (id: number) => {
-      if (window.confirm("Deseja realmente excluir?")) {
-        PessoasService.deleteById(id).then((result) => {
-          if (result instanceof Error) {
-            alert(result.message);
-          } else {
-            // alert(Environment.EXCLUIDO_COM_SUCESSO);
-            navigate("/pessoas");
-          }
+  const handleDelete = (id: number) => {
+    if (window.confirm("Realmente deseja apagar?")) {
+      PessoasService.deleteById(id).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
           alert("Registro apagado com sucesso!");
-        });
-      }
-    },
-    [navigate]
-  );
+          navigate("/pessoas");
+        }
+      });
+    }
+  };
 
   return (
     <LayoutBaseDePagina
-      titulo={id === "nova" ? "Nova Pessoa" : nome}
+      titulo={id === "nova" ? "Nova pessoa" : nome}
       barraDeFerramentas={
         <FerramentasDeDetalhe
           textoBotaoNovo="Nova"
           mostrarBotaoSalvarEFechar
           mostrarBotaoNovo={id !== "nova"}
           mostrarBotaoApagar={id !== "nova"}
-          aoClicarEmSalvar={() => formRef.current?.submitForm()}
-          aoClicarEmSalvarEFechar={() => formRef.current?.submitForm()}
-          aoClicarEmApagar={() => handleDelete(Number(id))}
           aoClicarEmVoltar={() => navigate("/pessoas")}
+          aoClicarEmApagar={() => handleDelete(Number(id))}
+          aoClicarEmSalvar={() => formRef.current?.submitForm()}
           aoClicarEmNovo={() => navigate("/pessoas/detalhe/nova")}
+          aoClicarEmSalvarEFechar={() => formRef.current?.submitForm()}
         />
       }
     >
-      {isLoading && <LinearProgress variant="indeterminate" />}
+      <Form ref={formRef} onSubmit={handleSave}>
+        <Box
+          margin={1}
+          display="flex"
+          flexDirection="column"
+          component={Paper}
+          variant="outlined"
+        >
+          <Grid container direction="column" padding={2} spacing={2}>
+            {isLoading && (
+              <Grid item>
+                <LinearProgress variant="indeterminate" />
+              </Grid>
+            )}
 
-      <Form
-        ref={formRef}
-        onSubmit={handleSave}
-        // placeholder={undefined} children={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
-      >
-        <VTextField
-          name="email"
-          label="Email"
-          placeholder="Email"
-          type="email"
-        />
+            <Grid item>
+              <Typography variant="h6">Geral</Typography>
+            </Grid>
 
-        <VTextField
-          name="nomeCompleto"
-          label="Nome Completo"
-          placeholder="Nome Completo"
-          type="text"
-        />
-        <VTextField
-          name="cidadeId"
-          label="Id Cidade"
-          placeholder="Cidade id"
-          type="number"
-        />
+            <Grid container item direction="row" spacing={2}>
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                <VTextField
+                  fullWidth
+                  name="nomeCompleto"
+                  disabled={isLoading}
+                  label="Nome completo"
+                  onChange={(e) => setNome(e.target.value)}
+                />
+              </Grid>
+            </Grid>
 
-        <button type="submit">Submit</button>
+            <Grid container item direction="row" spacing={2}>
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                <VTextField
+                  fullWidth
+                  name="email"
+                  label="Email"
+                  disabled={isLoading}
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container item direction="row" spacing={2}>
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                <VTextField
+                  fullWidth
+                  label="Cidade"
+                  name="cidadeId"
+                  disabled={isLoading}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
       </Form>
     </LayoutBaseDePagina>
   );
